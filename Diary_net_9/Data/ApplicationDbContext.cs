21575ae298d90c817e16a977140f2f86a7b9667c
Dariusz Okonski <darek200180@gmail.com>
2026-08-31 18:47:09 +0200

    add connection to the builder object

---
+++ Diary_net_9/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Diary_net_9.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        { 
+                            
+        }
+    }
+}
