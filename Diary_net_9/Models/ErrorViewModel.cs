064b2aa395ca5e986f3f1084d12ffe76a0269603
Dariusz Okonski <darek200180@gmail.com>
2026-08-31 06:54:14 +0200

    initialization

---
+++ Diary_net_9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Diary_net_9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
