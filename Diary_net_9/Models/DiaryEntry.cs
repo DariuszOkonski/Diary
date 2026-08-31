3af8cea088930dc32bc3ae2c5d41e704225a407d
Dariusz Okonski <darek200180@gmail.com>
2026-08-31 07:07:22 +0200

    make properties set up

--- Diary_net_9/Models/DiaryEntry.cs
+++ Diary_net_9/Models/DiaryEntry.cs
@@ -7,10 +7,10 @@ namespace Diary_net_9.Models
         [Key]
         public int Id { get; set; }
         [Required]
-        public string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
         [Required]
-        public string Content { get; set; }
+        public string Content { get; set; } = string.Empty;
         [Required]
-        public DateTime Created { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
     }
 }
