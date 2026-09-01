2670e38c16d6a6c3154d1d31c258094844be1cca
Dariusz Okonski <darek200180@gmail.com>
2026-09-01 07:29:28 +0200

    add seeding on ApplicationDbContext

---
+++ Diary_net_9/Migrations/20260901052800_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Diary_net_9.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe", new DateTime(2026, 9, 1, 7, 27, 59, 680, DateTimeKind.Local).AddTicks(7049), "Went Hiking" },
+                    { 2, "Went shopping with Joe", new DateTime(2026, 9, 1, 7, 27, 59, 680, DateTimeKind.Local).AddTicks(7053), "Went Shopping" },
+                    { 3, "Went diving with Joe", new DateTime(2026, 9, 1, 7, 27, 59, 680, DateTimeKind.Local).AddTicks(7057), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
