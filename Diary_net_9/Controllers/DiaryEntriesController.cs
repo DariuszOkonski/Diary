ce0a23fba0f222a0b72d7a37594bf6a7c68b8d44
Dariusz Okonski <darek200180@gmail.com>
2026-09-01 07:06:56 +0200

    add DiaryEntriesController

---
+++ Diary_net_9/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Diary_net_9.Controllers
+{
+    public class DiaryEntriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
