5e153549d25f1611981edc7bfbe1559aaa2060b4
Dariusz Okonski <darek200180@gmail.com>
2026-09-02 05:47:25 +0200

    display entries

--- Diary_net_9/Controllers/DiaryEntriesController.cs
+++ Diary_net_9/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Diary_net_9.Data;
+using Diary_net_9.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Diary_net_9.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> objDiaryEntryList = _db.DiaryEntries.ToList();
+
+            return View(objDiaryEntryList);
         }
     }
 }
